c0a62d4868d5b32b6b48977e9d84895023017ece
sang274 <truongsang2072004@gmail.com>
2026-02-25 21:46:36 +0700

    add controller

--- SDLS.Model/Models/SdlsDbContext.cs
+++ SDLS.Model/Models/SdlsDbContext.cs
@@ -1,6 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
 
 namespace SDLS.Model.Models;
 
@@ -103,9 +104,40 @@ public partial class SdlsDbContext : DbContext
 
     public virtual DbSet<Verification> Verifications { get; set; }
 
+    //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+    //        => optionsBuilder.UseNpgsql("Host=ep-silent-water-a15kqusf-pooler.ap-southeast-1.aws.neon.tech;Port=5432;Database=neondb;Username=neondb_owner;Password=npg_YTl1rgocn7XU;SSL Mode=Require;Trust Server Certificate=true");
+
+    public static string GetConnectionString(string connectionStringName = "DefaultConnection")
+    {
+        var basePath = Directory.GetCurrentDirectory();
+
+        var config = new ConfigurationBuilder()
+            .SetBasePath(basePath)
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+            .Build();
+
+        var connectionString = config.GetConnectionString(connectionStringName);
+
+        if (string.IsNullOrWhiteSpace(connectionString))
+        {
+            throw new InvalidOperationException(
+                $"Connection string '{connectionStringName}' not found in appsettings.json or environment-specific file.");
+        }
+
+        return connectionString;
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=ep-silent-water-a15kqusf-pooler.ap-southeast-1.aws.neon.tech;Port=5432;Database=neondb;Username=neondb_owner;Password=npg_YTl1rgocn7XU;SSL Mode=Require;Trust Server Certificate=true");
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var connString = GetConnectionString("DefaultConnection");
+            optionsBuilder.UseNpgsql(connString);
+        }
+    }
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- SDLS.Services/Interfaces/IQuestionService.cs
+++ SDLS.Services/Interfaces/IQuestionService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SDLS.Model.DTOs.Question;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace SDLS.Services.Interfaces
 {
     public interface IQuestionService
     {
+        Task<QuestionDTO> GetByIdAsync(Guid id);
+        Task<IEnumerable<QuestionDTO>> GetAllAsync();
+        Task<QuestionDTO> CreateAsync(QuestionCreateDTO dto);
+        Task<QuestionDTO> UpdateAsync(Guid id, QuestionUpdateDTO dto);
+        Task DeleteAsync(Guid id);
     }
 }
