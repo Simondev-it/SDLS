8286e8be07ba34610ec8e48e4ab931b5b931f82a
sang274 <truongsang2072004@gmail.com>
2026-03-20 13:59:23 +0700

    question tag DTO

---
+++ SDLS.Model/DTOs/QuestionTag/QuestionTagCreateDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDLS.Model.DTOs.QuestionTag
+{
+    public class QuestionTagCreateDTO
+    {
+        public Guid QuestionId { get; set; }
+
+        public Guid TagId { get; set; }
+    }
+}
---
+++ SDLS.Model/DTOs/QuestionTag/QuestionTagUpdateDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDLS.Model.DTOs.QuestionTag
+{
+    public class QuestionTagUpdateDTO
+    {
+        public Guid Id { get; set; }
+
+        public Guid QuestionId { get; set; }
+
+        public Guid TagId { get; set; }
+        
+        public int? Status { get; set; }
+    }
+}
