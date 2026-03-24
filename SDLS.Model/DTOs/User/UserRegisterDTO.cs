c1753ea49a94d94b52ce529fffc8afa6cd422802
Simondev-it <Duongntse180440@fpt.edu.vn>
2026-03-24 16:06:33 +0700

    update constans

--- SDLS.Model/DTOs/User/UserRegisterDTO.cs
+++ SDLS.Model/DTOs/User/UserRegisterDTO.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SDLS.Model.DTOs.User
 {
-    internal class UserDTO
+    public class UserRegisterDTO
     {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+
+        public string? Name { get; set; }
+        public string? Avatar { get; set; }
+        public string? Phone { get; set; }
+        public string? Gender { get; set; }
+        public DateOnly? DateOfBirth { get; set; }
     }
 }
