7dadca942ad6400b4d4303ba2c6d80aec9aafb0a
sang274 <truongsang2072004@gmail.com>
2026-04-03 15:25:12 +0700

    fix role const

--- SDLS.Model/Constants/RoleConst.cs
+++ SDLS.Model/Constants/RoleConst.cs
@@ -8,12 +8,12 @@ namespace SDLS.Model.Constants
 {
     public static class RoleConst
     {
-        public static readonly Guid USER_ROLE_ID = Guid.Parse("b944491c-af9f-485c-aa88-3671314dcc04");
+        public static readonly Guid USER_ROLE_ID = Guid.Parse("66efa899-db8f-4866-bfe9-6fe9d98eca7e");
 
-        public static readonly Guid GUEST_ROLE_ID = Guid.Parse("b944491c-af9f-485c-aa88-3671314dcc04");
+        public static readonly Guid GUEST_ROLE_ID = Guid.Parse("66efa899-db8f-4866-bfe9-6fe9d98eca7e");
         
-        public static readonly Guid INSTRUCTOR_ROLE_ID = Guid.Parse("77c846e8-c1dd-43e5-9165-d7967d550543");
+        public static readonly Guid INSTRUCTOR_ROLE_ID = Guid.Parse("179599cd-c7c9-4b3b-b0c7-f832991dc7a4");
 
-        public static readonly Guid ADMIN_ROLE_ID = Guid.Parse("622655ad-39f0-4c48-ae85-c4a6dfb2ef83"); 
+        public static readonly Guid ADMIN_ROLE_ID = Guid.Parse("90292570-fb35-445c-9f1f-a37dcdd48e5e"); 
     }
 }
