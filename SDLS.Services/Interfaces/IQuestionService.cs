72e4689eb3d58c7e8f9f131cad1ea5862e61a16f
sang274 <truongsang2072004@gmail.com>
2026-04-04 10:56:54 +0700

    gắn ApiException cho •	QuestionLessonService •	QuestionService •	QuestionTopicService

--- SDLS.Services/Interfaces/IQuestionService.cs
+++ SDLS.Services/Interfaces/IQuestionService.cs
@@ -23,8 +23,6 @@ namespace SDLS.Services.Interfaces
         Task<bool> CreateAsync(QuestionCreateDTO dto);
         Task<bool> CreateManyAsync(List<QuestionCreateDTO> dtos);
         Task<bool> UpdateAsync(Guid id, QuestionUpdateDTO dto);
-
-        Task<bool> DeleteAsync(Guid id);
         Task<bool> DeleteSoftAsync(Guid id);
         Task<bool> DeleteHardAsync(Guid id);
     }
